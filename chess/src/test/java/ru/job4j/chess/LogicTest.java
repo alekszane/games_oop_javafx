45591489b873fb4e617928351a6dc2c66fbc96a7
Alekszane <alekszane@gmail.com>
2021-03-03 00:23:55 +0300

    add tests for exceptions

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -7,13 +7,12 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class LogicTest {
 
-    @Ignore
     @Test
     public void whenMoveFree()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.H6);
+        logic.move(Cell.C1, Cell.G5);
     }
 
     @Test (expected = ImpossibleMoveException.class)
@@ -22,5 +21,22 @@ public class LogicTest {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H8);
+    }
+
+    @Test (expected = FigureNotFoundException.class)
+    public void whenNotFindFigureInCell()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.D2, Cell.H6);
+    }
+
+    @Test (expected = OccupiedCellException.class)
+    public void whenMoveNotFree()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new BishopBlack(Cell.F4));
+        logic.move(Cell.C1, Cell.H6);
     }
 }
